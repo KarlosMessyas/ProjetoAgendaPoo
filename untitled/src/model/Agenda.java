d0c386b0a0f41e68172f7e6c0c2b625577295713
Karlos Messyas <karlosmessyas14@gmail.com>
2023-10-27 07:54:36 -0300

    Modelando as primeira classes do projeto

---
+++ untitled/src/model/Agenda.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Agenda {
+    private List<Contato> contatos;
+
+    public Agenda(){
+        contatos = new ArrayList<>();
+    }
+
+    public boolean adicionar(Contato contato){
+        return contatos.add(contato);
+    }
+
+
+
+    //Listar os contatos
+    public List<Contato> getContatos() {
+        return contatos;
+    }
+}
